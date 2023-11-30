561b558a0557ec43eeb9869c73992ca8ecd2b71a
lmyzey <55479928+lmyzey@users.noreply.github.com>
2023-11-30 20:36:09 +0800

    创建项目和添加模块

---
+++ passjava-question/src/test/java/com/edu/ruc/passjavaquestion/PassjavaQuestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.ruc.passjavaquestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassjavaQuestionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
